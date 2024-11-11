7e6e3a45beecf1ba1feb6dd799ea3808e12bb087
AnthonyTerreros <dterrero@espol.edu.ec>
2024-11-11 11:35:36 -0500

    Fix: Modelos en el DataContext

--- Backend/PuntoDeVenta/Data/DataContext.cs
+++ Backend/PuntoDeVenta/Data/DataContext.cs
@@ -15,13 +15,21 @@ namespace PuntoDeVenta.Data
 
         public DbSet<Bodega> Bodegas { get; set; }
 
-        public DbSet<CLiente> Clientes { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         public DbSet<Proveedores> Proveedores { get; set; }
 
-        public DbSet<Sede> Proveedores { get; set; }
+        public DbSet<Sede> Sede { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Permisos> Permisos { get; set; }
+
+        public DbSet<Roles> Roles { get; set; }
+
+        public DbSet<UserRol> UsersRol { get; set; }
+
+        public DbSet<UserRolPermisos> UsersRolPermisos { get; set; }
 
     }
 }
